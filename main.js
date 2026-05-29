e796344ea3477709b9fa2425ad69a374fe463a4d
shsu0002 <shsu0002@student.monash.edu>
2026-05-29 11:27:38 +1000

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const STOPS = [
       {country:'China',     pct:29.5, color:'#1d7a68'},
       {country:'Malaysia',  pct:6.4,  color:'#c94030'},
       {country:'India',     pct:4.5,  color:'#7f6ab8'},
-      {country:'Hong Kong', pct:2.5,  color:'#b87c2a'},
+      {country:'Hong Kong', pct:2.5,  color:'#b87c2a'}, 
       {country:'Vietnam',   pct:1.9,  color:'#378add'},
     ],
     rest: '58', chart2Type:'lollipop',
